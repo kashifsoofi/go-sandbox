232fe9892ca1caf207554417b3ec801ff1da7411
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-06-30 12:03:10 +0100

    Implement timer for quiz

--- quiz-part-2/main.go
+++ quiz-part-2/main.go
@@ -6,11 +6,12 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
-
+	timeLimit := flag.Int("timeLimit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 
 	f, err := os.Open(*csvFilename)
@@ -27,14 +28,31 @@ func main() {
 
 	problems := parseProblems(records)
 
+	fmt.Printf("Press any key to start quiz, you have %d seconds to complete.\n", *timeLimit)
+	var r rune
+	fmt.Scanf("%r", &r)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
+
+problemloop:
 	for i, p := range problems {
 		fmt.Printf("Problem #%d: %s = \n", i+1, p.question)
+		answerCh := make(chan string)
 
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.answer {
-			correct++
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			break problemloop
+		case answer := <-answerCh:
+			if strings.ToLower(strings.TrimSpace(answer)) == p.answer {
+				correct++
+			}
 		}
 	}
 
@@ -51,7 +69,7 @@ func parseProblems(records [][]string) []problem {
 	for i, r := range records {
 		problems[i] = problem{
 			question: r[0],
-			answer:   strings.TrimSpace(r[1]),
+			answer:   strings.ToLower(strings.TrimSpace(r[1])),
 		}
 	}
 	return problems
